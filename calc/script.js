3e0b44963d8b94186e498d84dadb9c6f91feec90
さとしいぬ3104 <85342392+satoshiinu@users.noreply.github.com>
2022-04-16 00:08:30 +0900

    Update script.js

--- calc/script.js
+++ calc/script.js
@@ -1,3 +1,6 @@
+//初期化
+onminis=false
+
 function input(input) {
   var input_field = document.getElementById('input_field');
   if (input_field.value[input_field.value.length] ==".") {
@@ -16,4 +19,8 @@ function math(math) {
   var input_field = document.getElementById('input_field');
   var input_field2 = document.getElementById('input_field2');
   input_field2.value =input_field.value
+}
+
+function minus(minus) {
+  onminis=!minis
 }
