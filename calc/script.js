c9fe8c3c279af20ecbd2b003933d86bb5e5c287c
さとしいぬ3104 <85342392+satoshiinu@users.noreply.github.com>
2022-04-15 22:46:41 +0900

    Update script.js

--- calc/script.js
+++ calc/script.js
@@ -2,7 +2,7 @@ function input(input) {
   var input_field = document.getElementById('input_field');
   input_field.value =""+input_field.value+input
 }
-function delete(type) {
+function char_delete(type) {
   var input_field = document.getElementById('input_field');
   input_field.value=input_field.value.slice(0,input_field.value.length-1)
 }
