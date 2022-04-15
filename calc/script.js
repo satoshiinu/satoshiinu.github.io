13ca332db475414dd06f0bbd5186f2b89685de44
さとしいぬ3104 <85342392+satoshiinu@users.noreply.github.com>
2022-04-16 00:35:03 +0900

    Update script.js

--- calc/script.js
+++ calc/script.js
@@ -4,24 +4,25 @@ onminus=false
 function input(input) {
   var input_field = document.getElementById('input_field');
   if (onminus) {
-    input_field.value =""+input_field.value+"."+input
+    input_field.value =""+input_field.value+"."+input;
   } else {
-    input_field.value =""+input_field.value+input
+    input_field.value =""+input_field.value+input;
   }
 }
 
 function char_delete(type) {
   var input_field = document.getElementById('input_field');
-  input_field.value=input_field.value.slice(0,input_field.value.length-1)
+  input_field.value=input_field.value.slice(0,input_field.value.length-1);
 }
 
 function math(math) {
   var input_field = document.getElementById('input_field');
   var input_field2 = document.getElementById('input_field2');
-  input_field2.value =input_field.value
+  eval("input_field2",math,"input_fleld");
+  input_field2.value =input_field.value;
 }
 
 function minus(minus) {
-  onminus=!(minus)
-  alert(onminus)
+  onminus=!(minus);
+  alert(onminus);
 }
