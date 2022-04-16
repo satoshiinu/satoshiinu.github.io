7fc6ca31bbd3df157e2167162e2328488a8f934c
さとしいぬ3104 <85342392+satoshiinu@users.noreply.github.com>
2022-04-16 19:01:12 +0900

    Update script.js

--- calc/script.js
+++ calc/script.js
@@ -29,6 +29,7 @@ function char_delete(type) {
         input_field.value = input_field.value.slice(0,input_field.value.length-1);
       }
     }
+  }
   if ( type == 2) {
      var input_field = document.getElementById('input_field');
      input_field = 0 ;
