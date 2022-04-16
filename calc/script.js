b277370063d569ad6651666fbfc694a0d9bc92ed
さとしいぬ3104 <85342392+satoshiinu@users.noreply.github.com>
2022-04-16 20:08:09 +0900

    Update script.js

--- calc/script.js
+++ calc/script.js
@@ -24,9 +24,9 @@ function char_delete(type) {
     } else {
       var input_field = document.getElementById('input_field');
       if ( input_field.value[input_field.value.length-2] == "." ) {
-        input_field.value = input_field.value.slice(0,input_field.value.length-2);
+        input_field.value = input_field.value.slice( 0,input_field.value.length-2 );
       } else {
-        input_field.value = input_field.value.slice(0,input_field.value.length-1);
+        input_field.value = input_field.value.slice( 0,input_field.value.length-1 );
       }
     }
   }
@@ -66,7 +66,6 @@ function math2(smath) {
     var input_field = document.getElementById('input_field');
     var input_field2 = document.getElementById('input_field2');
     if ( smath !== "**" ) {
-    input_field.value = eval(input_field2.value + smath + input_field.value);
     } else {
     input_field.value = Math.pow( input_field2.value , input_field.value );
     }
