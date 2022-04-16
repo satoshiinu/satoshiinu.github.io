9f233f966c0611e305981bb93a21a396e5d4c3bc
さとしいぬ3104 <85342392+satoshiinu@users.noreply.github.com>
2022-04-16 10:00:05 +0900

    Update script.js

--- calc/script.js
+++ calc/script.js
@@ -1,26 +1,26 @@
 //初期化
-onminus=false
-placeholder=false
+onminus = false;
+placeholder = false;
 
 function input(input) {
   var input_field = document.getElementById('input_field');
   if (onminus && !(input_field.value.test)) {
-    input_field.value =""+input_field.value+"."+input;
+    input_field.value = ""+input_field.value+"."+input;
   } else {
-    input_field.value =""+input_field.value+input;
+    input_field.value = ""+input_field.value+input;
   }
 }
 
 function char_delete(type) {
   var input_field = document.getElementById('input_field');
-  input_field.value=input_field.value.slice(0,input_field.value.length-1);
+  input_field.value = input_field.value.slice(0,input_field.value.length-1);
 }
 
 function math(math) {
   var input_field = document.getElementById('input_field');
   var input_field2 = document.getElementById('input_field2');
-  input_field.value =eval(input_field2.value+math+input_field.value);
-  input_field2.value =input_field.value;
+  input_field.value = eval(input_field2.value+math+input_field.value);
+  input_field2.value = input_field.value;
 }
 
 function minus(minus) {
@@ -29,3 +29,6 @@ function minus(minus) {
 }
 
 function placeholderdisabled() {
+  placeholder = false;
+  var input_field = document.getElementById('input_field');
+  input_field.value = "";
