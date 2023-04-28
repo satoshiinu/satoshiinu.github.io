604945693a46700cdfcdf987e0c67d4cd59eb906
さとしいぬ <85342392+satoshiinu@users.noreply.github.com>
2023-04-28 20:16:42 +0900

    Add files via upload

--- games/script.js
+++ games/script.js
@@ -1,30 +1,70 @@
-//画面に表示される時は4倍になるので注意
+//エラーメッセージ処理
+window.onerror = (message, file, lineNo, colNo, error) => {
+    alert("エラーが発生しました\n再読み込みしてください");
+}
 
+//画面に表示される時は4倍になるので注意
 const zoom = 4
 
-var player = new Object();
-player.x = 25 * 16
-player.y = 25 * 16
-player.xsp = 0
-player.ysp = 0
+//その他の変数の作成
+timer = 0;
 
+//プレイヤーの変数の作成
+var player = new Object();
+player.x = 25 * 16;
+player.y = 25 * 16;
+player.xspd = 0;
+player.yspd = 0;
+player.scrollx = player.x + 160;
+player.scrolly = player.y + 24;
+player.scroll_offsetx = 0;
+player.scroll_offsety = 0;
+player.image = undefined;
+player.anim = 0;
+player.rotate = 0;
+player.canRotate = true;
+
+//キーの変数の作成
 var key = new Object();
 key.up = false;
 key.down = false;
 key.right = false;
 key.left = false;
 
-var img = new Object();
-img.player = new Image();
-img.player.src = "img/player.png"
-img.tile = new Image();
-img.tile.src = "img/tile.png"
+//画像の設定
+img = new Object();
 
-if (document.getElementById('btnUp').onmousedown) console.log("juianf")
+img.tiles = new Image();
+img.tiles.src = "img/tiles.png";
+
+img.players = new Image();
+img.players.src = "img/players.png";
+
+//タイルの設定
+var tile_image_list = [img.empty, img.tile, img.tile2];
+
+var tile_collision = [false, true, false];
+
+//
+const tileAtlas = [[0, 0], [1, 0], [2, 0]];
+const playerAtlas = [[0, 0], [1, 0], [2, 0]];
 
 
+//キャンバスのやつ
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = 320 * zoom;
+canvas.height = 180 * zoom;
+
+//アンチエイリアスを無効にする
+ctx.mozImageSmoothingEnabled = false;
+ctx.webkitImageSmoothingEnabled = false;
+ctx.msImageSmoothingEnabled = false;
+ctx.imageSmoothingEnabled = false;
+
+//マップデータ
 var map = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -47,18 +87,25 @@ var map = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -76,61 +123,67 @@ var map = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 ]
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-
-//アンチエイリアスを無効にする
-ctx.mozImageSmoothingEnabled = false;
-ctx.webkitImageSmoothingEnabled = false;
-ctx.msImageSmoothingEnabled = false;
-ctx.imageSmoothingEnabled = false;
-
 function main() {
     //キャンバスの初期化
-    ctx.clearRect(0, 0, 1280, 720)
+    ctx.clearRect(0, 0, 1280 * zoom, 720 * zoom)
 
-    //キー
+    //キー判定
     addEventListener("keyup", keyupfunc, false);
     addEventListener("keydown", keydownfunc, false);
 
     //移動
-    if (player.xsp > 0) {
-        player.xsp = Math.floor(player.xsp * 0.85 * 1000) / 1000;
+    if (player.xspd > 0) {
+        player.xspd = Math.floor(player.xspd * 0.85 * 1000) / 1000;
     } else {
-        player.xsp = Math.ceil(player.xsp * 0.85 * 1000) / 1000;
+        player.xspd = Math.ceil(player.xspd * 0.85 * 1000) / 1000;
     }
-    if (player.ysp > 0) {
-        player.ysp = Math.floor(player.ysp * 0.85 * 1000) / 1000;
+    if (player.yspd > 0) {
+        player.yspd = Math.floor(player.yspd * 0.85 * 1000) / 1000;
     } else {
-        player.ysp = Math.ceil(player.ysp * 0.85 * 1000) / 1000;
-    }
-
-    if (key.up) player.ysp -= 0.5;
-    if (key.down) player.ysp += 0.5;
-    if (key.right) player.xsp += 0.5;
-    if (key.left) player.xsp -= 0.5;
-
-    if (player.xsp > 0) {
-        player.x += Math.floor(player.xsp);
-    } else {
-        player.x += Math.ceil(player.xsp);
-    }
-    if (player.ysp > 0) {
-        player.y += Math.floor(player.ysp);
-    } else {
-        player.y += Math.ceil(player.ysp);
+        player.yspd = Math.ceil(player.yspd * 0.85 * 1000) / 1000;
     }
 
+    if (key.up) player.yspd -= 0.5;
+    if (key.down) player.yspd += 0.5;
+    if (key.right) player.xspd += 0.5;
+    if (key.left) player.xspd -= 0.5;
+
+    //プレイヤー移動
+    player_move(player.xspd, player.yspd, true);
+
+    if(player.canRotate) rotate()
+
+    //スクロール座標を取得
+    player.scrollx = player.x - 160 + player.scroll_offsetx
+    player.scrolly = player.y - 80 + player.scroll_offsety
+
+    //タイル描画
     for (var y = 0; y < 13; y++) {
         for (var x = 0; x < 21; x++) {
-            if (map[y + Math.floor(player.y / 16)][x + Math.floor(player.x / 16)] == 0) ctx.drawImage(img.tile, 0, 0, 16, 16, (x * 16 + (16 - player.x % 16) - 16) * zoom, (y * 16 + (16 - player.y % 16) - 16) * zoom, 16 * zoom, 16 * zoom);
+            var tileID = map[y + Math.floor(player.scrolly / 16)][x + Math.floor(player.scrollx / 16)]
+            ctx.drawImage(img.tiles, getTileAtlasXY(tileID, 0), getTileAtlasXY(tileID,1), 16, 16, (x * 16 + (16 - player.scrollx % 16) - 16) * zoom, (y * 16 + (16 - player.scrolly % 16) - 16) * zoom, 16 * zoom, 16 * zoom);
         }
     }
 
-    //プレイヤー描画
-    ctx.drawImage(img.player, 0, 0, 16, 24, 160 * zoom, 90 * zoom, 16 * zoom, 24 * zoom);
+    //プレイヤー画像指定
+    if(Math.abs(player.xspd) + Math.abs(player.yspd) < 3) {
+        player.anim = 2;
+    } else {
+        if(timer % 50 > 0000) player.anim = 0;
+        if(timer % 50 > 12.5) player.anim = 2;
+        if(timer % 50 > 25.0) player.anim = 1;
+        if(timer % 50 > 37.5) player.anim = 2;
+    }
+    //player.image = eval("img.player."+player.rotate+"_"+player.anim);//本当はeval使いたくない
 
-    console.log("x:" + player.x + ",y:" + player.y + ",xsp:" + player.xsp + ",ysp:" + player.ysp)
+    //プレイヤー描画
+    ctx.drawImage(img.players, player.anim * 16 , player.rotate * 32, 16, 24, (player.x - player.scrollx) * zoom, (player.y - player.scrolly - 8) * zoom, 16 * zoom, 24 * zoom);
+
+    //
+    console.log("x:" + player.x + ",y:" + player.y + ",xspd:" + player.xspd + ",yspd:" + player.yspd)
+
+    //タイマー
+    timer++;
     //ループ
     requestAnimationFrame(main);
 }
@@ -151,6 +204,70 @@ function keyupfunc(parameter) {
     if (key_code == 38) key.up = false;
     if (key_code == 39) key.right = false;
     if (key_code == 40) key.down = false;
+}
+
+//プレイヤーの動き
+function player_move(mvx, mvy, checkhitbox) {
+    for (var i=0 ; i < Math.round(Math.abs(mvx)) ; i++) {
+        player.x += Math.sign(mvx);
+        if (hitbox(player.x, player.y) && checkhitbox ) player.x -= Math.sign(mvx);
+    }
+    for (var i=0 ; i < Math.round(Math.abs(mvy)) ; i++) {
+        player.y += Math.sign(mvy);
+        if (hitbox(player.x, player.y) && checkhitbox ) player.y -= Math.sign(mvy);
+    }
+}
+
+//当たり判定
+function hitbox(x, y) {
+    if (tile_collision[map[Math.floor(y / 16 + 0   )][Math.floor(x / 16 + 0   )]]) return true;
+    if (tile_collision[map[Math.floor(y / 16 + 0   )][Math.floor(x / 16 + 0.95)]]) return true;
+    if (tile_collision[map[Math.floor(y / 16 + 0.95)][Math.floor(x / 16 + 0   )]]) return true;
+    if (tile_collision[map[Math.floor(y / 16 + 0.95)][Math.floor(x / 16 + 0.95)]]) return true;
+}
+
+//向きを取得
+function rotate() {
+    if ( key.up && !key.down && !key.right && !key.left) player.rotate = 4;
+    if (!key.up &&  key.down && !key.right && !key.left) player.rotate = 0;
+    if (!key.up && !key.down &&  key.right && !key.left) player.rotate = 6;
+    if (!key.up && !key.down && !key.right &&  key.left) player.rotate = 2;
+    if ( key.up && !key.down &&  key.right && !key.left) player.rotate = 5;
+    if ( key.up && !key.down && !key.right &&  key.left) player.rotate = 3;
+    if (!key.up &&  key.down &&  key.right && !key.left) player.rotate = 7;
+    if (!key.up &&  key.down && !key.right &&  key.left) player.rotate = 1;
+    if (!key.up && !key.down &&  key.right &&  key.left) player.rotate = 4;
+    if (!key.up && !key.down &&  key.right &&  key.left) player.rotate = 0;
+    if ( key.up &&  key.down && !key.right && !key.left) player.rotate = 6;
+    if ( key.up &&  key.down && !key.right && !key.left) player.rotate = 2;
+    if ( key.up && !key.down &&  key.right &&  key.left) player.rotate = 4;
+    if (!key.up &&  key.down &&  key.right &&  key.left) player.rotate = 0;
+    if ( key.up &&  key.down &&  key.right && !key.left) player.rotate = 2;
+    if ( key.up &&  key.down && !key.right &&  key.left) player.rotate = 6;
+    if ( key.up &&  key.down &&  key.right &&  key.left) player.rotate = 4;
+}
+
+function getTileAtlasXY(id, xy) {
+    return tileAtlas[id][xy] * 16
+}
+
+function getPlayerAtlasXY(id, xy) {
+    return playerAtlas[id][xy] * 16
+}
+
+
+function hexColour(c) {
+    if (c < 256) {
+        return Math.abs(c).toString(16);
+    }
+    return 0;
+}
+
+function decColour(c) {
+    if (c < 256) {
+        return Math.abs(c).toString(10);
+    }
+    return 0;
 }
 
 function btnUpDown() {
