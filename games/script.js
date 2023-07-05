b49bba2d9980b5a87959ee5075adaf499d0e8bc3
さとしいぬ <85342392+satoshiinu@users.noreply.github.com>
2023-07-05 21:34:36 +0900

    Add files via upload

--- games/script.js
+++ games/script.js
@@ -2,6 +2,8 @@
 // Copyright 2023 satoshiinu. All rights reserved. //
  ///////////////////////////////////////////////////
 
+//"use strict"
+
 //エラーメッセージ処理
 var DoSayErrorMassege = true;
 window.onerror = (message, file, lineNo, colNo, error) => {
@@ -9,10 +11,15 @@ window.onerror = (message, file, lineNo, colNo, error) => {
 }
 
 //画面に表示される時は4倍になるので注意
-const zoom = 4
+const zoom = 4;
 
 //その他の変数の作成
 var timer = 0;
+
+//デバッグ用の変数の作成
+var debug = new Object();
+debug.hitbox_visual = true;
+debug.hitboxes = new Array();
 
 //プレイヤーの変数の作成
 var player = new Object();
@@ -32,9 +39,10 @@ player.up = false
 player.down = false
 player.right = false
 player.left = false
+player.attack = 0;
 
 //敵の変数の作成
-var enemy = [];
+var enemy = new Array();
 
 enemy_spawn(25, 25);
 enemy_spawn(25, 26);
@@ -42,26 +50,22 @@ enemy_spawn(20, 20);
 
 //キーの変数の作成
 var key = new Object();
-key.up = false;
-key.down = false;
-key.right = false;
-key.left = false;
-key.w = false;
-key.a = false;
-key.s = false;
-key.d = false;
-key.z = false;
-key.x = false;
-key.c = false;
 
 var keys = new Array();
 
 for (let i = 0; i < 255; i++) {
     keys[i] = {
-        "press" : false,
-        "timer":0
+        "press": false,
+        "pressLag": false,
+        "down": false,
+        "timer": 0,
+        "time": -1
     }
 }
+
+//キー判定
+addEventListener("keyup", keyupfunc);
+addEventListener("keydown", keydownfunc);
 
 //画像の設定
 var img = new Object();
@@ -110,7 +114,7 @@ const playerAtlas = [ 0, 1, 2];
 
 //マップ読み込み
 var loadedjson = new Object();
-loadJson("Map.json", "Map");
+loadJson("param/maps/Map.json", "Map");
 //console.log("LOADED");
 
 //メッセージ変数作成
@@ -131,39 +135,16 @@ ctx.msImageSmoothingEnabled = false;
 ctx.imageSmoothingEnabled = false;
 
 
-function main() {keyreset()
+
+function main() {
     //キャンバスの初期化
-    ctx.clearRect(0, 0, 1280 * zoom, 720 * zoom)
+    ctx.clearRect(0, 0, 320 * zoom, 180 * zoom);
 
+    //プレイヤーの動作
+    player_proc();
 
-    //キー判定
-    addEventListener("keyup", keyupfunc, false);
-    addEventListener("keydown", keydownfunc, false);
-
-    //移動
-    if (player.xspd > 0) {
-        player.xspd = Math.floor(player.xspd * 0.85 * 1000) / 1000;
-    } else {
-        player.xspd = Math.ceil(player.xspd * 0.85 * 1000) / 1000;
-    }
-    if (player.yspd > 0) {
-        player.yspd = Math.floor(player.yspd * 0.85 * 1000) / 1000;
-    } else {
-        player.yspd = Math.ceil(player.yspd * 0.85 * 1000) / 1000;
-    }
-
-    if (player.up) player.yspd -= 0.5;
-    if (player.down) player.yspd += 0.5;
-    if (player.right) player.xspd += 0.5;
-    if (player.left) player.xspd -= 0.5;
-
-    //プレイヤー移動
-    player_move(player.xspd, player.yspd, true);
-
-    if(player.canRotate) rotate()
-
-    //敵移動
-    if(true) enemy_move();
+    //敵の動作
+    if(true) enemy_proc();
 
     //スクロール座標を取得
     player.scrollx = player.x - 160 + player.scroll_offsetx;
@@ -174,7 +155,8 @@ function main() {keyreset()
     if (player.scrolly > 1420) player.scrolly = 1420;
 
     //メッセージ消す
-    if (keys[90].timer == 1) message.visible = false;
+    if (keys[90].time == timer) message.visible = false;
+
 
     //タイル描画
     var plx = Math.floor(player.scrollx / 16);
@@ -190,13 +172,19 @@ function main() {keyreset()
     }
 
     //プレイヤー画像指定
-    if(Math.abs(player.xspd) + Math.abs(player.yspd) < 3) {
+    if (Math.abs(player.xspd) + Math.abs(player.yspd) < 3) {
         player.anim = 2;
-    } else {
-        if(timer % 50 > 0.00) player.anim = 0;
-        if(timer % 50 > 12.5) player.anim = 2;
-        if(timer % 50 > 25.0) player.anim = 1;
-        if(timer % 50 > 37.5) player.anim = 2;
+    }
+    else {
+        if (player.attack > 0) {
+            player.anim = 2;
+        }
+        else {
+            if (timer % 50 > 0.00) player.anim = 0;
+            if (timer % 50 > 12.5) player.anim = 2;
+            if (timer % 50 > 25.0) player.anim = 1;
+            if (timer % 50 > 37.5) player.anim = 2;
+        }
     }
     //player.image = eval("img.player."+player.rotate+"_"+player.anim);//本当はeval使いたくない
 
@@ -222,12 +210,28 @@ function main() {keyreset()
         }
     }
 
+    ctx.strokeStyle = `rgb(
+        ${hsv2rgb([timer * 2 % 360, 1, 1])[0]},
+        ${hsv2rgb([timer * 2 % 360, 1, 1])[1]},
+        ${hsv2rgb([timer * 2 % 360, 1, 1])[2]}`
+
+    ctx.strokeStyle = "black"
+
+    //当たり判定描画
+    for (const i in debug.hitboxes) {
+        ctx.strokeRect( (debug.hitboxes[i][0] - player.scrollx) * zoom, (debug.hitboxes[i][1] - player.scrolly) * zoom, debug.hitboxes[i][2] * zoom, debug.hitboxes[i][3] * zoom );
+        //console.log(debug.hitboxes[i][0] + " " + debug.hitboxes[i][1] + " " + debug.hitboxes[i][2] + " " + debug.hitboxes[i][3]);
+        //console.log(debug.hitboxes[i]);
+    }
+    debug.hitboxes.splice(0);
+
+    debug_proc();
 
     //メッセージ描画
     if (message.visible) {
         //ctx.drawImage(img.gui.message, 0, 0, 16 * 16, 4 * 16, 32 * zoom, 96 * zoom, 16 * 16 * zoom, 4 * 16 * zoom);
         drawPrompt(5 * 8,13 * 8,32 * 8,8 * 8);
-        drawText(message.text);
+        drawText(message.text,40,104,31);
     }
     
     //console.log("x:" + player.x + ",y:" + player.y + ",xspd:" + player.xspd + ",yspd:" + player.yspd)
@@ -243,28 +247,27 @@ function main() {keyreset()
 requestAnimationFrame(main);
 
 
-function keyreset() {
-    for (let i = 0; i < 255; i++) {
-        keys[i].down = false
-        keys[i].up = false
-    }
-}
 
 function keydownfunc(parameter) {
     var key_code = parameter.keyCode;
+    if (keys[key_code].press) keys[key_code].pressLag = true;
     keys[key_code].press = true;
-    keys[key_code].timer ++;
+    keys[key_code].timer++;
+    if (keys[key_code].press && !keys[key_code].pressLag) keys[key_code].time = timer;
+    if (keys[key_code].press && !keys[key_code].pressLag) keys[key_code].down = true;
     parameter.preventDefault();
 }
 
 function keyupfunc(parameter) {
     var key_code = parameter.keyCode
     keys[key_code].press = false;
+    keys[key_code].pressLag = false;
+    keys[key_code].down = false;
     keys[key_code].timer = 0;
 }
 
 //プレイヤーの動き
-function player_move(mvx, mvy, checkhitbox) {
+function move_func(mvx, mvy, checkhitbox) {
     for (var i=0 ; i < Math.round(Math.abs(mvx)) ; i++) {
         player.x += Math.sign(mvx);
         if (hitbox(player.x, player.y) && checkhitbox ) player.x -= Math.sign(mvx);
@@ -343,7 +346,7 @@ function setMapData(json,name) {
 function readMapData(filePath,name) {
     var i = [];
     //console.log(filePath);
-    if (filePath.indexOf(';') == -1) {
+    if (filePath.indexof(';') == -1) {
 
         try {
             fetch(filePath)
@@ -361,10 +364,64 @@ function readMapData(filePath,name) {
     return map.map;
 }
 
-function enemy_move(){
+function player_proc() {
+
+    player_move()
+    player_attack()
+
+}
+
+function player_move() {
+
+    //移動
+    if (player.xspd > 0) {
+        player.xspd = Math.floor(player.xspd * 0.85 * 1000) / 1000;
+    } else {
+        player.xspd = Math.ceil(player.xspd * 0.85 * 1000) / 1000;
+    }
+    if (player.yspd > 0) {
+        player.yspd = Math.floor(player.yspd * 0.85 * 1000) / 1000;
+    } else {
+        player.yspd = Math.ceil(player.yspd * 0.85 * 1000) / 1000;
+    }
+
+    if (player.up) player.yspd -= 0.5;
+    if (player.down) player.yspd += 0.5;
+    if (player.right) player.xspd += 0.5;
+    if (player.left) player.xspd -= 0.5;
+
+    //プレイヤー移動
+    move_func(player.xspd, player.yspd, true);
+
+    if (player.canRotate) rotate()
+}
+
+function player_attack() {
+
+    var weapon_hitbox_x = 6;
+    var weapon_hitbox_y = 16;
+
+    if (player.rotate == 0 && player.attack != 0) if (hitbox_entity_rect(player.x + 00, player.y + 16, weapon_hitbox_x, weapon_hitbox_y)) console.log("hit!");
+    if (player.rotate == 1 && player.attack != 0) if (hitbox_entity_rect(player.x - 16, player.y + 04, weapon_hitbox_y, weapon_hitbox_x)) console.log("hit!");
+    if (player.rotate == 2 && player.attack != 0) if (hitbox_entity_rect(player.x + 08, player.y - 16, weapon_hitbox_x, weapon_hitbox_y)) console.log("hit!");
+    if (player.rotate == 3 && player.attack != 0) if (hitbox_entity_rect(player.x + 16, player.y + 04, weapon_hitbox_y, weapon_hitbox_x)) console.log("hit!");
+
+    if (player.attack != 0) player.attack++;
+    if (player.attack >= 25) player.attack = 0;
+    if (keys[32].time == timer) player.attack = 1;
+
+}
+
+function enemy_proc() {
+
     for (const i in enemy) {
+        enemy_move(i)
 
+        //if (hitbox_entity_rect(player.x, player.y, 16, 16)) console.log("hit!");
+    }
+}
 
+function enemy_move(i){
 
         if (enemy[i]["xspd"] > 0) {
             enemy[i]["xspd"] = Math.floor(enemy[i]["xspd"] * 0.85 * 1000) / 1000;
@@ -377,10 +434,12 @@ function enemy_move(){
             enemy[i]["yspd"] = Math.ceil(enemy[i]["yspd"] * 0.85 * 1000) / 1000;
         }
 
-        if (enemy[i]["move"][0]) enemy[i]["yspd"] -= 0.5;
-        if (enemy[i]["move"][1]) enemy[i]["yspd"] += 0.5;
-        if (enemy[i]["move"][2]) enemy[i]["xspd"] += 0.5;
-        if (enemy[i]["move"][3]) enemy[i]["xspd"] -= 0.5;
+        var enemy_speed = 0.25
+
+        if (enemy[i]["move"][0]) enemy[i]["yspd"] -= enemy_speed;
+        if (enemy[i]["move"][1]) enemy[i]["yspd"] += enemy_speed;
+        if (enemy[i]["move"][2]) enemy[i]["xspd"] += enemy_speed;
+        if (enemy[i]["move"][3]) enemy[i]["xspd"] -= enemy_speed;
         if (enemy[i]["move"][4] > 0) enemy[i]["move"][4] -= 1;
 
         //console.log("id: " + i + ",move: " + enemy.move[i][4]);
@@ -409,7 +468,7 @@ function enemy_move(){
             enemy[i]["y"] += Math.sign(enemy[i]["yspd"]);
             if (hitbox(enemy[i]["x"], enemy[i]["y"])) enemy[i]["y"] -= Math.sign(enemy[i]["yspd"]);
         }
-    }
+    
 }
 
 function enemy_spawn(spx, spy) { 
@@ -420,7 +479,13 @@ function enemy_spawn(spx, spy) {
         "y": spy * 16,
         "xspd": 0,
         "yspd": 0,
-        "move": [false, false, false, false, 0]
+        "move": [false, false, false, false, 0],
+        "attack": {
+            "hostility": [
+                false,
+                0
+            ]
+        }
     }
     //データの追加
     enemy.push(def);
@@ -453,18 +518,25 @@ function messageView(text) {
 
 }
 
-function NumberOfIndex(num,index,shinsu) {
+function NumberofIndex(num,index,shinsu) {
     return (String(num.toString(shinsu))[index]);
 }
 
-function drawText(text) {
+function drawText(text,x,y,w,h) {
 
-    for (let i = 0; i < text.length; i++) {
+    for (let i = 0, ox = 0 , oy = 0; i < text.length; i++) {
         var codeP = ("0000" + text.codePointAt(i).toString(16)).slice(-4);
 
-        ctx.drawImage(img.font["uni_" + NumberOfIndex(codeP, 0, 16) + NumberOfIndex(codeP, 1, 16) ], parseInt(NumberOfIndex(codeP, 3, 16), 16) * 8, parseInt(NumberOfIndex(codeP, 2, 16), 16) * 8, 8, 8, ((i * 8) + 40) * zoom, 104 * zoom, 8 * zoom, 8 * zoom);
+        try {
+            ctx.drawImage(img.font["uni_" + NumberofIndex(codeP, 0, 16) + NumberofIndex(codeP, 1, 16)], parseInt(NumberofIndex(codeP, 3, 16), 16) * 8, parseInt(NumberofIndex(codeP, 2, 16), 16) * 8, 8, 8, (x + ox * 8) * zoom, (y + oy * 8 )* zoom, 8 * zoom, 8 * zoom);
+        } catch { }
 
-        //console.log("x:" + parseInt(NumberOfIndex(codeP, 2, 16), 16) + ", y:" + parseInt(NumberOfIndex(codeP, 3, 16), 16));
+        ox++;
+        if (typeof w != 'undefined' && w < ox) {
+            oy++;
+            ox = 0;
+        }
+        //console.log("x:" + parseInt(NumberofIndex(codeP, 2, 16), 16) + ", y:" + parseInt(NumberofIndex(codeP, 3, 16), 16));
     }
 }
 
@@ -484,4 +556,133 @@ function drawPrompt(dx,dy,dw,dh) {
     ctx.drawImage(img.gui.prompt, 16, 16, 8, 8, (dx + dw) * zoom, (dy + dh) * zoom, 8 * zoom, 8 * zoom);
 
     ctx.drawImage(img.gui.prompt, 8, 8, 8, 8, dx * zoom, dy * zoom, dw * zoom, dh * zoom);
+}
+
+function hitbox_rect(ax, ay, aw, ah, bx, by, bw, bh) {
+
+    debug_hitbox_push(ax, ay, aw, ah);
+    debug_hitbox_push(bx, by, bw, bh);
+
+    var acx = ax + aw / 2;
+    var acy = ay + ay / 2;
+    var bcx = bx + bw / 2;
+    var bcy = by + by / 2;
+
+    var dx = Math.abs(acx - bcx);
+    var dy = Math.abs(acy - bcy);
+
+    var sx = (aw + bw) / 2
+    var sy = (ah + bh) / 2
+
+    if (dx < sx && dy < sy) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function hitbox_reci(ax, ay, aw, ah, bx, by, bw, bh) {
+
+    debug_hitbox_push(ax, ay, aw, ah);
+    debug_hitbox_push(bx, by, bw, bh);
+
+	var da = ax - bx;
+	var db = ay - by;
+    var dc = Math.sqrt(da ** 2 + db ** 2);
+
+    var ar = 2 / Math.sqrt(2 * Math.max(aw, ah));
+    var br = 2 / Math.sqrt(2 * Math.max(bw, bh));
+
+    if (dc <= ar + br) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function hitbox_entity_rect(ax, ay, aw, ah) {
+
+    var entity_hitbox_x = 16;
+    var entity_hitbox_y = 16;
+
+    for (const i in enemy) {
+        if (hitbox_rect(enemy[i]["x"], enemy[i]["y"], entity_hitbox_x, entity_hitbox_y, ax, ay, aw, ah)) return i
+    }
+    return false
+}
+
+
+function debug_hitbox_push(a,b,c,d) {
+
+    if(!debug.hitbox_visual) return
+
+    //データの作成
+    var def = [
+        a,b,c,d
+    ]
+    
+    //データの追加
+    debug.hitboxes.push(def);
+}
+
+function debug_proc() {
+    if (keys[75].time == timer) player.y++;
+    if (keys[73].time == timer) player.y--;
+    if (keys[76].time == timer) player.x++;
+    if (keys[74].time == timer) player.x--;
+
+    drawText("a:" + player.attack, 0, 0);
+    drawText("x:" + player.x, 0, 8);
+    drawText("y:" + player.y, 0, 16);
+
+    for (var i = 0 , j = 0; i < keys.length; i++) {
+        if (keys[i].press) drawText(String.fromCharCode(i), 78 * zoom, j * 8);
+        if (keys[i].press) j++
+    }
+}
+
+function hsv2rgb(hsv) {// あざす https://lab.syncer.jp/Web/JavaScript/Snippet/67/
+    var h = hsv[0] / 60;
+    var s = hsv[1];
+    var v = hsv[2];
+    if (s == 0) return [v * 255, v * 255, v * 255];
+
+    var rgb;
+    var i = parseInt(h);
+    var f = h - i;
+    var v1 = v * (1 - s);
+    var v2 = v * (1 - s * f);
+    var v3 = v * (1 - s * (1 - f));
+
+    switch (i) {
+        case 0:
+        case 6:
+            rgb = [v, v3, v1];
+            break;
+
+        case 1:
+            rgb = [v2, v, v1];
+            break;
+
+        case 2:
+            rgb = [v1, v, v3];
+            break;
+
+        case 3:
+            rgb = [v1, v2, v];
+            break;
+
+        case 4:
+            rgb = [v3, v1, v];
+            break;
+
+        case 5:
+            rgb = [v, v1, v2];
+            break;
+    }
+
+    return rgb.map(function (value) {
+        return value * 255;
+    });
 }
