38247f95a0e41c6c4c3375e4e9d2f6f9cd26b693
さとしいぬ <85342392+satoshiinu@users.noreply.github.com>
2023-11-22 21:53:56 +0900

    Add files via upload

---
+++ tool/LinearSimulEqua/script.js
@@ -0,0 +1,62 @@
+class Num {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    multi(e) {
+        this.x *= e;
+        this.y *= e;
+    }
+    get_multi(e) {
+        return new Num(this.x * e, this.y * e);
+    }
+    getArray(){
+        return [this.x,this.y];
+    }
+}
+
+class SimulEquaClass {
+    add(one, two) {
+        return new Num(one.x + two.x, one.y + two.y);
+    }
+}
+SimulEqua = new SimulEquaClass();
+
+class Result {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+    getArray(){
+        return [this.a,this.b];
+    }
+}
+
+function linearFuncCalc(one, two) {
+
+    let result = new Object();
+    let result_a = new Number();
+    let result_b = new Number();
+
+    result = SimulEqua.add(one, two.get_multi(-1));
+    result.multi(-1);
+    result_a = result.y / result.x;
+
+    result_b = -(result_a * one.x - one.y);
+
+    return new Result(result_a, result_b);
+}
+
+function getNumByElem(xID,yID){
+    let NumX=document.getElementById(xID).value*1;
+    let NumY=document.getElementById(yID).value*1;
+
+    return new Num(NumX,NumY);
+}
+
+function setNumByElem(xID,yID,xValue,yValue){
+    document.getElementById(xID).value=xValue;
+    document.getElementById(yID).value=yValue;
+
+    return 0;
+}
